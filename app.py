bb380c8d480dd3cd58e945444fa6534c6179793c
Prany25 <118735414+Prany25@users.noreply.github.com>
2025-05-31 19:38:41 +0530

    Final streamlit app that creates document embeddings and answers queries from it

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ('GROQ_API_KEY') = os.getenv("GROQ_API_KEY")
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 groq_api_key=os.getenv("GROQ_API_KEY")
 
 llm=ChatGroq(groq_api_key=groq_api_key,model_name="distil-whisper-large-v3-en")
@@ -34,12 +34,30 @@ def create_vector_embedding():
         st.session_state.embeddings=OllamaEmbeddings()
         st.session_state.loader=PyPDFDirectoryLoader("/research_papers")
         st.session_state.docs=st.session_state.loader.load()
-        st.session_state.text_splitter=RecursiveCharacterTextSplutter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
         st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
 
-prompt=st.text_input("Enter your query from the research paper")
+user_prompt=st.text_input("Enter your query from the research paper")
 
 if st.button("Document Embedding"):
     create_vector_embedding()
     st.write("Vector database is ready")
+
+import time
+
+if user_prompt:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':user_prompt})
+    print(f"Response time :{time.process_time()-start}")
+
+    st.write(response['answer'])
+
+    with st.expander("Document similarity Search"):
+        for i,doc in enumerate(response['context']):
+            st.write(doc.page_content)
+
